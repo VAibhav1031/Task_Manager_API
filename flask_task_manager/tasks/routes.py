ddaafddc9becf707ab0778f56f271ad422293db7
Vaibhav Tiwari <vaibhavgrand2004@gmail.com>
2025-10-08 18:04:45 +0530

    Improving the query parameter working

--- flask_task_manager/tasks/routes.py
+++ flask_task_manager/tasks/routes.py
@@ -36,16 +36,18 @@ logger = logging.getLogger(__name__)
 def parse_query_date(value: str, end_of_day: bool = False):
     dt = None
     try:
-        if len(value) == 10:  # DD-MM-YYYY
+        if len(value) == 10:  # YYYY-DD-MM
             dt = datetime.strptime(value, "%Y-%m-%d")
-            dt = dt.replace(
-                hour=24, minute=59, second=59 if end_of_day else 0, tzinfo=timezone.utc
-            )
-        elif len(value) == 16:  # YYYY-MM-DDTHH:MM:
-            dt = datetime.strptime(value, "%Y-%m-%d")
-            dt = dt.replace(second=59 if end_of_day else 0,
-                            tzinfo=timezone.utc)
-
+            if end_of_day:
+                dt = dt.replace(
+                    hour=23,
+                    minute=59,
+                    second=59,
+                    tzinfo=timezone.utc,
+                )
+            else:
+                dt = dt.replace(hour=0, minute=0, second=0,
+                                tzinfo=timezone.utc)
         else:
             # whaat if  frontend guy said lets give the whole date time , but if he missed the timezone stamp
             dt = parser.isoparse(value)
@@ -72,21 +74,16 @@ def filter_manager(completion, title: str, after_str: str, before_str: str, quer
             # It is  still object which is return but that object has peice of info that will use when  compare in query
             # Z is stand for the 'ZULU' which is bit common for the military and all and normal use also , python library doesnt have any way
             # to handle that or something so we replace that thing with notrmal one s
-            after = parse_query_date(after_str)
+            after = parse_query_date(after_str, end_of_day=False)
         if before_str:
-            before = parse_query_date(before_str)
+            before = parse_query_date(before_str, end_of_day=True)
 
     except Exception as e:
-        logger.error(
-            f"Invalid datetime format {
-                e}, Use ISO 8601 UTC , eg 2025-09-11T18:30:00Z"
-        )
+        logger.error(f"Invalid datetime format {e}")
         return bad_request(
             type="InvalidDatetime",
             msg="Invalid Datetime used other than the standard one",
-            details=f"Invalid datetime format {
-                e
-            }, Use ISO 8601 UTC , eg 2025-09-11T18:30:00Z",
+            details=str(e),
         )
 
     if completion is not None:
@@ -95,6 +92,7 @@ def filter_manager(completion, title: str, after_str: str, before_str: str, quer
             query = query.filter(Task.completion.is_(True))
         elif normalized in ("false", "0", "no"):
             query = query.filter(Task.completion.is_(False))
+
     # postgres based
     # if title is not None:
     #     query = query.filter(Task.title.ilike(f"%{title}%"))
