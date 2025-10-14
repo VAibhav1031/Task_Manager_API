79532d83687f96080a1d2b2bed492121fb88b33a
Vaibhav Tiwari <vaibhavgrand2004@gmail.com>
2025-10-14 11:49:34 +0530

    Updated docker-entrypoint.sh scripy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,14 +4,14 @@ set -e
 
 source ./.venv/bin/activate
 
-until pg_isready --host=dev-db --port=5432 --dbname=${DEV_DB_NAME} --username=${DEV_DB_USER}; do
+until pg_isready --host=prod-db --port=5432 --dbname=${DB_NAME} --username=${DB_USER}; do
   echo "Waiting for the database to be ready.."
   sleep 2
 done
 
 echo "Database is ready!!"
 
-export PGPASSWORD=${DEV_DB_PASSWORD}
+export PGPASSWORD=${DB_PASSWORD}
 # # cause flask  app container is different then the dev-db , so for the psql command in postgres  connection to local peer is Trust no password
 # # but since flask app contaiener run on different ip (network) so it need the TCP for connection for that defualt is md5 which is password requirement
 
@@ -19,7 +19,7 @@ echo "Setting up...."
 if [ -d migrations/versions ] && [ "$(ls -A migrations/versions)" ]; then
 
   echo "checking model SYNC..."
-  if ! psql -h dev-db --port=5432 -U ${DEV_DB_USER} -d ${DEV_DB_NAME} -tAc "SELECT 1 FROM information_schema.tables WHERE table_name='alembi_version'" | grep -q 1; then
+  if ! psql -h prod-db --port=5432 -U ${DB_USER} -d ${DB_NAME} -tAc "SELECT 1 FROM information_schema.tables WHERE table_name='alembi_version'" | grep -q 1; then
     flask --app run.py db stamp head
   fi
 
@@ -36,7 +36,7 @@ else
 fi
 
 echo "Startting the flask application...."
-exec uv run python run.py
+exec uv run gunicorn -w 4 -b 0.0.0.0:5555 run:app
 
 # flow is  like this   check_if_db_is_ready -> check_migration_folder -> if the migrations script not there -> create_initials & migrate -> db upgrade --> run application
 
